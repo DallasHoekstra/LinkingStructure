2cc7c7f4005ff7a88d6e0a127f8a65097e1fb7d7
hoekstradev <hoekstradev@gmail.com>
2021-04-27 18:38:21 -0400

    Tie previous methods together with extract_page_structure function. Strip whitespace on titles.

--- LinkingStructure.py
+++ LinkingStructure.py
@@ -20,17 +20,30 @@ def download_url(target_url):
     elif re.search('^[4-5][0-9][0-9]$', response.status) is not None:
         raise Exception(f'Error received for URL: {URL}')
 
-def extract_links(html_page):
+def extract_links_and_title(html_page):
     soup = BeautifulSoup(html_page, 'html.parser')
 
     links = []
     for anchor in soup.find_all("a"):
         links.append(anchor['href'])
-    return links
+    title = soup.find("title").text.strip()
+    return (links, title)
 
 def generate_page(page_title, link_list):
     page = graph.Page(page_title)
     for link in link_list:
         page.add_link(link)
+    return page
+
+def extract_page_structure(target_url):
+    try:
+        response = download_url(target_url)
+    except:
+        #log error
+        pass
+    
+    link_list, page_title = extract_links_and_title(response.text)
+    page = generate_page(page_title, link_list)
+
     return page
 
--- LinkingStructureTests.py
+++ LinkingStructureTests.py
@@ -20,6 +20,9 @@ class Response_Object():
 def generate_sample_html():
         sample_page = """<html> 
                             <head>
+                                <title>
+                                    Search Provider
+                                </title>
                             </head>
                             <body>
                                 <a href="www.google.com">Begin the search!</a>
@@ -31,7 +34,7 @@ def generate_sample_html():
         f.close()
 
 @patch("requests.get")
-def test_opens_correct_initial_url(mock_requests_get):
+def test_download_urls_opens_correct_initial_url(mock_requests_get):
     mock_requests_get.return_value = Response_Object(status='200', data='google')
     target_url = "https://www.google.com"
 
@@ -42,19 +45,20 @@ def test_opens_correct_initial_url(mock_requests_get):
 
 @pytest.mark.parametrize("status", [("404"), ("400"), ("500")])
 @patch("requests.get")
-def test_handles_error_codes_correctly(mock_requests_get, status):
+def test_download_urls_handles_error_codes_correctly(mock_requests_get, status):
 
     mock_requests_get.return_value = Response_Object(status=status, data='')
     target_url = "https://www.google.com"
     with pytest.raises(Exception):
         LS.download_url(target_url)
     
-def test_extract_links_extracts_anchor_tag_hrefs():
+def test_extract_links_and_title_extracts_anchor_tag_hrefs_and_title():
     if not path.exists('sample_html.html'):
         generate_sample_html()
 
     with open('sample_html.html', 'r') as sample_page:
-        links = LS.extract_links(sample_page)
+        links, title = LS.extract_links_and_title(sample_page)
+        assert title == "Search Provider"
         assert links is not None
 
 def test_generate_page_adds_links_and_title():
@@ -67,3 +71,16 @@ def test_generate_page_adds_links_and_title():
     assert page.links["www.google.com"] == 1
     assert page.links["www.giantitp.com"] == 1
     assert page.links["www.xkcd.com"] == 2
+
+@patch("requests.get")
+def test_extract_page_structure_converts_url_to_page_object(mock_requests_get):
+    if not path.exists('sample_html.html'):
+        generate_sample_html()
+
+    with open('sample_html.html', 'r') as sample_page:
+        mock_requests_get.return_value = Response_Object(status='200', data=sample_page)
+    
+        page = LS.extract_page_structure("www.google.com")
+    
+        assert page.title == "Search Provider"
+        assert page.links["www.google.com"] == 1
