bdaeca3773cf8ecd81899caeaa74dee2c2cb266f
hoekstradev <hoekstradev@gmail.com>
2021-04-27 18:10:05 -0400

    Add link extraction and page object generation

--- LinkingStructure.py
+++ LinkingStructure.py
@@ -4,21 +4,33 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import WebGraph as graph
 # import time
 # import json
 # import csv
 
 # Initial URL to target
 def download_url(target_url):
-    URL = target_url
+    response = requests.get(target_url)
 
-    response = requests.get(URL)
-    print(response)
     if re.search('^[2-3][0-9][0-9]$', response.status) is not None:
-        print('Success')
+        return response
     elif response.status == '404':
         raise Exception(f'404 Error received for URL: {URL}')
     elif re.search('^[4-5][0-9][0-9]$', response.status) is not None:
         raise Exception(f'Error received for URL: {URL}')
 
+def extract_links(html_page):
+    soup = BeautifulSoup(html_page, 'html.parser')
+
+    links = []
+    for anchor in soup.find_all("a"):
+        links.append(anchor['href'])
+    return links
+
+def generate_page(page_title, link_list):
+    page = graph.Page(page_title)
+    for link in link_list:
+        page.add_link(link)
+    return page
 
--- LinkingStructureTests.py
+++ LinkingStructureTests.py
@@ -1,7 +1,10 @@
 import pytest
 import unittest
+import os.path
+from os import path
 from unittest.mock import patch
 import LinkingStructure as LS
+
 
 # def se_echo(*args, **kwargs):
 #     if len(args):
@@ -14,7 +17,18 @@ class Response_Object():
         self.status = kwargs['status']
         self.text = kwargs['data']
 
+def generate_sample_html():
+        sample_page = """<html> 
+                            <head>
+                            </head>
+                            <body>
+                                <a href="www.google.com">Begin the search!</a>
+                            </body> 
+                    </html>"""
 
+        f = open('sample_html.html', 'w')
+        f.write(sample_page)
+        f.close()
 
 @patch("requests.get")
 def test_opens_correct_initial_url(mock_requests_get):
@@ -26,7 +40,6 @@ def test_opens_correct_initial_url(mock_requests_get):
     mock_requests_get.assert_called()
     mock_requests_get.assert_called_with(target_url)
 
-
 @pytest.mark.parametrize("status", [("404"), ("400"), ("500")])
 @patch("requests.get")
 def test_handles_error_codes_correctly(mock_requests_get, status):
@@ -36,3 +49,21 @@ def test_handles_error_codes_correctly(mock_requests_get, status):
     with pytest.raises(Exception):
         LS.download_url(target_url)
     
+def test_extract_links_extracts_anchor_tag_hrefs():
+    if not path.exists('sample_html.html'):
+        generate_sample_html()
+
+    with open('sample_html.html', 'r') as sample_page:
+        links = LS.extract_links(sample_page)
+        assert links is not None
+
+def test_generate_page_adds_links_and_title():
+    link_list = ["www.google.com", "www.giantitp.com", "www.xkcd.com", "www.xkcd.com"]
+    page_title = "best websites"
+
+    page = LS.generate_page(page_title, link_list)
+
+    assert page.title == page_title
+    assert page.links["www.google.com"] == 1
+    assert page.links["www.giantitp.com"] == 1
+    assert page.links["www.xkcd.com"] == 2
